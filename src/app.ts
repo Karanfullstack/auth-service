016a4332dcfc8845a69fdc1d69e8c41ac0fe2458
Karan Chauhan <karanchouhan211@gmail.com>
2024-11-24 23:47:46 +0100

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,18 @@ import authRouter from './routes/auth';
 import tenantRouter from './routes/tenant';
 import userRouter from './routes/user';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(
+    cors({
+        origin: ['http://localhost:5173'],
+        credentials: true,
+    }),
+);
 app.use('/auth', authRouter);
 app.use('/tenants', tenantRouter);
 app.use('/users', userRouter);
